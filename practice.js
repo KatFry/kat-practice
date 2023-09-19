5bde79c85f3e475ac9f51a0246f616110164871f
Katherine Fry <katgfry@me.com>
2023-09-19 15:28:16 -0400

    c55

--- practice.js
+++ practice.js
@@ -1,6 +1,5 @@
 // this is my first comment to practice pushing changes to github
 // adding another comment to practice pushing
-// c47
 // c48
 // c49
 // c50
@@ -8,3 +7,4 @@
 // c52
 // c53
 // c54
+// c55
