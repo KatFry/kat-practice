b42cb0fee5476654e8f991b694f8059229f41ad7
Katherine Fry <katgfry@me.com>
2023-09-28 10:04:28 -0400

    c94

--- practice.js
+++ practice.js
@@ -1,12 +1,12 @@
 // this is my first comment to practice pushing changes to github
 // adding another comment to practice pushing
-// c87
 // c88
 // c89
 // c90
 // c91
 // c92
 // c93
+// c94
 
 //addTwo function
 function addTwo(num) {
