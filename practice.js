219adb211886667d5ba518cd74289557c14b4a79
Katherine Fry <katgfry@me.com>
2023-09-30 09:45:32 -0400

    objOfMatches

--- practice.js
+++ practice.js
@@ -58,3 +58,21 @@ const disemvowel = str => {
   }
   return result;
 }
+
+/*
+Construct a function objOfMatches that accepts two arrays and a callback. 
+objOfMatches will build an object and return it. 
+To build the object, objOfMatches will test each element of the first array 
+using the callback to see if the output matches the corresponding element (by index) of the second array. 
+If there is a match, the element from the first array becomes a key in an object, 
+and the element from the second array becomes the corresponding value.
+*/
+const objOfMatches = (arr1, arr2, cb) => {
+  const obj = {};
+  for (let i = 0; i < arr1.length; i++) {
+    if (cb(arr1[i]) === arr2[i]) {
+      obj[arr1[i]] = arr2[i];
+    }
+  }
+  return obj;
+}
