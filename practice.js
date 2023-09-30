78cf99ad43f89857feaa0917f38272ab41fb29b3
Katherine Fry <katgfry@me.com>
2023-09-30 09:50:33 -0400

    arrayBuilder

--- practice.js
+++ practice.js
@@ -94,3 +94,11 @@ const goodKeys = (obj, cb) => {
   }
   return result;
 }
+
+/*
+Write a function arrayBuilder that takes in a count object 
+and returns an array filled with the appropriate numbers of elements. 
+The order of the elements in the array does not matter, 
+but repeated elements should be grouped.
+*/
+
