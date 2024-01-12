c6e4c78a99495ec3b98228bd61ef165e6f9277a7
Katherine Fry <katgfry@me.com>
2024-01-12 16:07:48 -0500

    started kFrequentElements

---
+++ 5-kFrequentElements.js
@@ -0,0 +1,35 @@
+/*Given an integer array nums and an integer k, return the k most frequent elements. 
+You may return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+*/
+
+// input: nums (array of integers), k (integer)
+// output: array of numbers - most frequent elements in any element 
+
+const kFrequentElements = (nums, k) => {
+  // declare a const called arrayFrequency initialized to an empty array
+
+  // declare a const called frequencyObj initialized to an empty object 
+
+  // loop over the passed-in nums array 
+
+    // 
+
+  // return the arrayFrequency 
+}
