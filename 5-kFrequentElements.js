0e151053b3dee76e9424364a5ba9b9535fb86a7e
Katherine Fry <katgfry@me.com>
2024-01-18 16:15:04 -0500

    working on solution 1 to kFrequentElements

--- 5-kFrequentElements.js
+++ 5-kFrequentElements.js
@@ -22,14 +22,27 @@ It is guaranteed that the answer is unique.
 // input: nums (array of integers), k (integer)
 // output: array of numbers - most frequent elements in any element 
 
+// OPTION 1: Using a simple object for storage
 const kFrequentElements = (nums, k) => {
-  // declare a const called arrayFrequency initialized to an empty array
+  // declare a const called freqs assigned to an empty object
+  const freqs = {};
+  // loop over the nums array, storing how many times each num appears
+  for (const num of nums) {
 
-  // declare a const called frequencyObj initialized to an empty object 
-
-  // loop over the passed-in nums array 
-
-    // 
-
-  // return the arrayFrequency 
+  }
 }
+
+
+// const kFrequentElements = (nums, k) => {
+//   // declare a const assigned to a new instance of a Map 
+//   const frequencyMap = new Map(); 
+//   // loop over the passed-in nums array 
+//   for (const num of nums) {
+//     // declare a count variable to keep track of how frequent each num is in the array 
+//     // assign it to either using the get method to find corresponding num, or set to 0 if it doesn't exist yet 
+//     let count = frequencyMap.get(num) || 0; 
+//   }
+//     // 
+
+//   // return the arrayFrequency 
+// }
