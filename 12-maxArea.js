501bab759b9c301eb6ad2c878923d9fe994c66f7
Katherine Fry <katgfry@me.com>
2024-01-30 13:28:27 -0500

    12: tests passed

--- 12-maxArea.js
+++ 12-maxArea.js
@@ -51,6 +51,6 @@ const maxArea = height => {
   return maxArea;
 }
 
-/* // TESTS:
+// TESTS:
 console.log(maxArea([1,8,6,2,5,4,8,3,7])); // -> 49
-console.log(maxArea([1,1])); // -> 1 */
+console.log(maxArea([1,1])); // -> 1
