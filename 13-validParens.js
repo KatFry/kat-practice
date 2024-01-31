785c30397ae577525b2fcf7019a73a4ce1de70ca
Katherine Fry <katgfry@me.com>
2024-01-31 16:49:59 -0500

    13: time and space complexity

--- 13-validParens.js
+++ 13-validParens.js
@@ -23,22 +23,24 @@ Input: s = "(]"
 Output: false
 */
 
+/* time and space complexity: both O(n) */
+
 // input: string
 // output: boolean (if parens are valid or not)
 const validParens = s => {
   // declare a const initialized to an empty stack
-  const stack = [];
+  const stack = []; 
   // iterate through the characters in the input string
   for (let i = 0; i < s.length; i++) {
     // declare const char assigned to s at i to provide readability 
     const char = s[i];
     // if the character is an opening bracket: (, {, or [, push it to the stack
-    if (char === '(' || char === '{' || char === '[') stack.push(s[i]);
+    if (char === '(' || char === '{' || char === '[') stack.push(char); 
     else {
       // if the stack is empty, return false
       if (!stack.length) return false;
       // assign a constant popped to popping the element off the stack
-      const popped = stack.pop();
+      const popped = stack.pop(); 
       // check if the characters match the closing brackets 
       // if the char is ')' and popped is not equal to '(' OR
       if (
@@ -57,7 +59,8 @@ const validParens = s => {
 }
 
 
-// TESTS: 
+/* // TESTS: 
 console.log(validParens('()')); // -> true
 console.log(validParens('()[]{}')); // -> true
 console.log(validParens('(]')); // -> false 
+ */
