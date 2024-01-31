ea4198a1a9285cb50035712a909dcc6861e93b49
Katherine Fry <katgfry@me.com>
2024-01-31 16:45:05 -0500

    13: finished and tests

--- 13-validParens.js
+++ 13-validParens.js
@@ -23,13 +23,41 @@ Input: s = "(]"
 Output: false
 */
 
+// input: string
+// output: boolean (if parens are valid or not)
 const validParens = s => {
-
+  // declare a const initialized to an empty stack
+  const stack = [];
+  // iterate through the characters in the input string
+  for (let i = 0; i < s.length; i++) {
+    // declare const char assigned to s at i to provide readability 
+    const char = s[i];
+    // if the character is an opening bracket: (, {, or [, push it to the stack
+    if (char === '(' || char === '{' || char === '[') stack.push(s[i]);
+    else {
+      // if the stack is empty, return false
+      if (!stack.length) return false;
+      // assign a constant popped to popping the element off the stack
+      const popped = stack.pop();
+      // check if the characters match the closing brackets 
+      // if the char is ')' and popped is not equal to '(' OR
+      if (
+        (char === ')' && popped !== '(') ||
+        // if the char is '}' and popped is not equal to '{' OR
+        (char === '}' && popped !== '{') || 
+        // if the char is ']' and popped is not equal to '[', return false 
+        (char === ']' && popped !== '[') 
+      ) {
+        return false;
+      }
+    }
+  }
+  // if the stack is empty, return true (there are no remaining open brackets in the stack) 
+  return stack.length === 0;
 }
 
 
-/* // TESTS: 
+// TESTS: 
 console.log(validParens('()')); // -> true
 console.log(validParens('()[]{}')); // -> true
 console.log(validParens('(]')); // -> false 
-*/
