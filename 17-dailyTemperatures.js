2456619cd84a34a9498c3884c0e1e30bd4af2fcd
Katherine Fry <katgfry@me.com>
2024-02-07 17:18:16 -0500

    17: tests passed

--- 17-dailyTemperatures.js
+++ 17-dailyTemperatures.js
@@ -49,7 +49,6 @@ const dailyTemperatures = (temps) => {
 
 
 /* // TESTS:
-console.log(dailyTemperatures()); // -> 
-console.log(dailyTemperatures()); // -> 
-console.log(dailyTemperatures()); // ->  
-*/
+console.log(dailyTemperatures([73,74,75,71,69,72,76,73])); // -> [1,1,4,2,1,1,0,0]
+console.log(dailyTemperatures([30,40,50,60])); // -> [1,1,1,0]
+console.log(dailyTemperatures([30,60,90])); // -> [1,1,0] */
