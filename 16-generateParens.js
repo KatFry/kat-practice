ebdd4f362fd05f825096508a839cbc102898c6bc
Katherine Fry <katgfry@me.com>
2024-02-07 16:35:03 -0500

    16: question and tests

---
+++ 16-generateParens.js
@@ -0,0 +1,22 @@
+/* 
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+Input: n = 1
+Output: ["()"]
+*/
+
+// input: number
+// output: array of strings - possible valid parens generated 
+const generateParens = n => {
+
+}
+
+/* // TESTS:
+console.log(generateParens(3)); // -> ["((()))","(()())","(())()","()(())","()()()"]
+console.log(generateParens(1)); // -> ["()"]
+*/
