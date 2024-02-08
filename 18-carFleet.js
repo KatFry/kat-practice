f60c75cf778e27d298c20164d6c122f2d1a3437b
Katherine Fry <katgfry@me.com>
2024-02-08 11:21:31 -0500

    18: time and space complexity

--- 18-carFleet.js
+++ 18-carFleet.js
@@ -44,6 +44,16 @@ Then, the fleet (speed 2) and the car starting at 4 (speed 1) become one fleet,
 meeting each other at 6. The fleet moves at speed 1 until it reaches target.
 */
 
+/* time and space complexity:
+- time: "linearithmic"
+  - sorting: O(n log n) - where n is the number of cars ("linearithmic" - combo of linear and logarithmic (O(log n))
+  - iteration through cars: O(n) - where n is the number of cars
+  - overall: dominant factor is sorting stop, so O(n log n)
+- space: linear 
+  - array of car objects: O(n) - where n is number of cars
+  - overall: O(n) 
+*/
+
 // input: target (number), position (array), speed (array) 
 // output: number (number of car fleets that will arrive at destination)
 const carFleet = (target, position, speed) => {
