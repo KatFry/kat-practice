57598f274b3ae81b757fec8000eea47167eba7a7
Katherine Fry <katgfry@me.com>
2024-02-09 16:16:19 -0500

    22: other strategy that works with test cases

--- 22-searchMatrix.js
+++ 22-searchMatrix.js
@@ -21,6 +21,55 @@ const searchMatrix = (matrix, target) => {
   if (!matrix || matrix.length === 0 || matrix[0].length === 0) return false;
   // assign a const m to the length of the entire matrix (signifying rows of the matrix)
   const m = matrix.length;
+  // initialize variable left to the matrix at 0 at 0
+  let left = matrix[0][0];
+  // initialize a variable right to the matrix at m minus 1 at n minus 1 (last el - last row and last column)
+  let right = matrix[m - 1][matrix[m - 1].length - 1];
+  
+  // iterate while left is less than or equal to right 
+  while (left <= right) {
+    // assign a const mid to the left plus Math.floor of right minus left divided by 2 (avg of L and R)
+    const mid = left + Math.floor((right - left) / 2);
+      
+    // find the row and column indices for the mid value
+    let midRow = 0;
+    let midCol = 0;
+    for (let i = 0; i < m; i++) {
+      if (mid >= matrix[i][0] && mid <= matrix[i][matrix[i].length - 1]) {
+        midRow = i;
+        midCol = matrix[i].indexOf(mid);
+        break;
+      }
+    }
+      
+    // check: if the matrix at midRow at midCol equals the target, return true
+    if (matrix[midRow][midCol] === target) return true;
+      
+    // else if it is less than the target, reassign left to mid plus 1 (target may be in the right half)
+    else if (matrix[midRow][midCol] < target) left = mid + 1;
+      
+    // else reassign right to mid minus 1 (target may be in the left half) 
+    else right = mid - 1;
+  }
+    
+  // return false outside the loop if the target was never found 
+  return false;
+}
+
+
+/* // TESTS:
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)); // -> true
+console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)); // -> false
+ */
+
+
+/* for some reason the below solution doesn't work with the test cases and I'm not sure why...
+
+const searchMatrix = (matrix, target) => {
+  // initial check: if the matrix is empty or has no rows or columns, return false because target can't be found
+  if (!matrix || matrix.length === 0 || matrix[0].length === 0) return false;
+  // assign a const m to the length of the entire matrix (signifying rows of the matrix)
+  const m = matrix.length;
   // assign a const n to the length of the matrix at 0 (signifying columns in the matrix) 
   const n = matrix[0].length;
   // initialize variable left to the matrix at 0 at 0
@@ -45,8 +94,4 @@ const searchMatrix = (matrix, target) => {
   // return false outside the loop if the target was never found 
   return false;
 }
-
-/* // TESTS:
-console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)); // -> true
-console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)); // -> false
-*/
+ */
