622bc7e348c0e533d74a895a432e06966a818a5f
Katherine Fry <katgfry@me.com>
2024-02-09 15:10:10 -0500

    20: pseudocode binary search approach

--- 21-search.js
+++ 21-search.js
@@ -15,6 +15,8 @@ Output: -1
 Explanation: 2 does not exist in nums so return -1
 */
 
+/* OPTION 1: normal O(n) linear approach */
+
 // input: array of integers, target number
 // output: index of target, otherwise -1 
 const search = (nums, target) => {
@@ -30,4 +32,31 @@ const search = (nums, target) => {
 /* // TESTS:
 console.log(search([-1,0,3,5,9,12], 9)); // -> 4
 console.log(search([-1,0,3,5,9,12], 2)); // -> -1
- */
+*/
+
+/* OPTION 2: binary search approach (O log n - logarithmic) */
+
+const binSearch = (nums, target) => {
+  // initialize a variable left to 0 to indicate the start
+  
+  // initialize a variable right to nums.length minus 1 to indicate the end 
+
+  // iterate while left is less than or equal to right
+
+    // declare a constant mid assigned to Math.floor of left plus right divided by 2
+
+    // check: if nums at mid is the target, return mid
+
+    // else if nums at mid is less than the target, reassign left to mid plus 1 (target may be in right half)
+
+    // else reassign right to mid minus 1 (target may be in left half) 
+
+  // return -1 if the target was not found 
+
+}
+
+
+/* // TESTS:
+console.log(binSearch([-1,0,3,5,9,12], 9)); // -> 4
+console.log(binSearch([-1,0,3,5,9,12], 2)); // -> -1
+*/
