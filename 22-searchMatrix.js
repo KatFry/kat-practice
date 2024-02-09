93344bee77913af30cdc65d98426f0e69f664a68
Katherine Fry <katgfry@me.com>
2024-02-09 15:55:37 -0500

    22: pseudocode strategy

--- 22-searchMatrix.js
+++ 22-searchMatrix.js
@@ -17,6 +17,29 @@ Output: false
 */
 
 const searchMatrix = (matrix, target) => {
+  // assign a const m to the length of the entire matrix (signifying rows of the matrix)
+
+  // assign a const n to the length of the matrix at 0 (signifying the number of columns in the matrix) 
+
+  // initialize variable left to the matrix at 0 at 0
+
+  // initialize a variable right to the matrix at m minus 1 at n minus 1 (last row and last column)
+
+  // iterate while left is less than or equal to right 
+
+    // assign a const mid to the left plus Math.floor of right minus left divided by 2 
+
+    // assign a const midRow to Math.floor of mid divided by n 
+
+    // assign a const midCol to mid remainder n 
+
+    // check: if the matrix at midRow at midCol equals the target, return true
+
+    // else if the matrix at midRow at midCol is less than the target, reassign left to mid plus 1
+
+    // else reassign right to mid minus 1
+
+  // return false outside the loop if the target was never found 
 
 }
 
