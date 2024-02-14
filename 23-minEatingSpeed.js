ec275d1fae6615cc264955b02ca8a7f9f8012cb8
Katherine Fry <katgfry@me.com>
2024-02-14 12:04:22 -0500

    23: tests passed

--- 23-minEatingSpeed.js
+++ 23-minEatingSpeed.js
@@ -70,4 +70,4 @@ const minEatingSpeed = (piles, h) => {
 console.log(minEatingSpeed([3,6,7,11], 8)); // -> 4
 console.log(minEatingSpeed([30,11,23,4,20], 5)); // -> 30
 console.log(minEatingSpeed([30,11,23,4,20], 6)); // -> 23
-*/
+ */
