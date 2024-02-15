313f8934e767530246cb8e3992c934e2f61077e2
Katherine Fry <katgfry@me.com>
2024-02-15 09:49:32 -0500

    25: trying to fix quokka

--- 25-searchArray.js
+++ 25-searchArray.js
@@ -46,7 +46,8 @@ const searchArray = (nums, target) => {
 }
 
 
-// TESTS:
+/* // TESTS:
 console.log(searchArray([4,5,6,7,0,1,2], 0)); // -> 4
 console.log(searchArray([4,5,6,7,0,1,2], 3)); // -> -1
 console.log(searchArray([1], 0)); // -> -1
+ */
