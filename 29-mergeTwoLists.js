78882b9ed0c438df9aa531fee524147ed90018ab
Katherine Fry <katgfry@me.com>
2024-02-16 13:32:46 -0500

    29: pseudocode strategy

--- 29-mergeTwoLists.js
+++ 29-mergeTwoLists.js
@@ -17,8 +17,51 @@ Input: list1 = [], list2 = [0]
 Output: [0]
 */
 
-const mergeTwoLists = (list1, list2) => {
+// input: 2 sorted linked lists (arrays)
+// output: merged list as an array 
+
+// make a listNode class, passing in val and next to the constructor 
+class ListNode {
   
+}
+
+
+
+const mergeTwoLists = (list1, list2) => {
+  // declare a const dummy assigned to a new instance of the ListNode 
+
+  // assign a variable current to dummy (point to current node in merged list)
+
+  // assign a variable tail to dummy (point to tail of merged list) 
+
+  // simplify code by assigning variable p1 to list1, p2 to list2
+
+  // while p1 and p2 exist
+
+    // if the p1 value is less than the p2 value, 
+
+      // append smaller node to merged list by assigning current.next to p1
+
+      // move pointer in list1 by assigning p1 to p1.next
+
+    // else 
+
+      // assign current.next to p2 (append smaller node to merged list)
+
+      // move pointer in list2 by assigning p2 to p2.next 
+
+    // assign current to current.next to move to the next node in the merged list
+
+  // if one list becomes empty (like only p1 is left), assign current.next to p1
+
+  // else if only p2 is left, assign current.next to p2 
+
+  // find the tail of the merged list by looping while current.next exists
+
+    // assign current to current.next 
+
+  // return the head and tail of merged list (head is dummy.next, tail is current) 
+
 }
 
 
