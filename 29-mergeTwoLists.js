9cee92e9c16057522e1a43535d1ffd63b453c50d
Katherine Fry <katgfry@me.com>
2024-02-16 13:40:40 -0500

    29: code finished

--- 29-mergeTwoLists.js
+++ 29-mergeTwoLists.js
@@ -22,46 +22,52 @@ Output: [0]
 
 // make a listNode class, passing in val and next to the constructor 
 class ListNode {
-  
-}
-
-
+  constructor (val, next = null) {
+    this.val = val; 
+    this.next = next;
+  }
+};
 
 const mergeTwoLists = (list1, list2) => {
   // declare a const dummy assigned to a new instance of the ListNode 
-
+  const dummy = new ListNode();
   // assign a variable current to dummy (point to current node in merged list)
-
+  let current = dummy;
   // assign a variable tail to dummy (point to tail of merged list) 
-
+  let tail = dummy;
   // simplify code by assigning variable p1 to list1, p2 to list2
-
+  let p1 = list1;
+  let p2 = list2;
   // while p1 and p2 exist
-
+  while (p1 && p2) {
     // if the p1 value is less than the p2 value, 
-
+    if (p1.val < p2.val) {
       // append smaller node to merged list by assigning current.next to p1
-
+      current.next = p1;
       // move pointer in list1 by assigning p1 to p1.next
-
+      p1 = p1.next;
+    }
     // else 
-
+    else {
       // assign current.next to p2 (append smaller node to merged list)
-
+      current.next = p2;
       // move pointer in list2 by assigning p2 to p2.next 
-
+      p2 = p2. next;
+    }
     // assign current to current.next to move to the next node in the merged list
-
+    current = current.next;
+  }
   // if one list becomes empty (like only p1 is left), assign current.next to p1
-
+  if (p1) current.next = p1;
   // else if only p2 is left, assign current.next to p2 
-
+  else current.next = p2; 
   // find the tail of the merged list by looping while current.next exists
-
+  while (current.next) {
     // assign current to current.next 
-
+    current = current.next;
+  }
   // return the head and tail of merged list (head is dummy.next, tail is current) 
-
+  return { head: dummy.next, tail: current };
 }
 
 
