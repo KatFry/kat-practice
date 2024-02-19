f68aef5459ce2b8d173a763e4df57d3b4e627383
Katherine Fry <katgfry@me.com>
2024-02-19 13:30:59 -0500

    31: question and tests

---
+++ 31-removeNthFromEnd.js
@@ -0,0 +1,27 @@
+/* 
+Given the head of a linked list, remove the nth node from the end of the list
+and return its head.
+
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+Input: head = [1,2], n = 1
+Output: [1]
+*/
+
+const removeNthFromEnd = (head, n) => {
+
+};
+
+
+/* // TESTS:
+console.log(removeNthFromEnd([1,2,3,4,5], 2)); // -> [1,2,3,5]
+console.log(removeNthFromEnd([1], 1)); // -> []
+console.log(removeNthFromEnd([1,2], 1)); // -> [1]
+*/
