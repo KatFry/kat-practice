31a509276b95921a9bc42c6a1edaa479e9af1f7d
Katherine Fry <katgfry@me.com>
2024-02-19 13:37:03 -0500

    31: pseudocode strategy

--- 31-removeNthFromEnd.js
+++ 31-removeNthFromEnd.js
@@ -16,7 +16,29 @@ Output: [1]
 */
 
 const removeNthFromEnd = (head, n) => {
+  // create a dummy node for handling edge cases (like removing from head)
 
+  // assign dummy.next to the head
+
+  // initialize a variable first to dummy
+
+  // also initialize a variable second to dummy
+
+  // move the second pointer n + 1 steps ahead (looping while i <= n + 1)
+
+    // assign second to second.next
+
+  // move both pointers until second pointer reaches end (while second isn't null)
+  
+    // assign first to first.next
+
+    // assign second to second.next
+
+  // remove nth node from end by updating next pointer of preceding node 
+  // do this by assigning first.next to first.next.next
+
+  // return dummy.next (the modified head)
+  
 };
 
 
