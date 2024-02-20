4f731a9f33e09db19b705239698a60b27bd8c131
Katherine Fry <katgfry@me.com>
2024-02-20 14:19:31 -0500

    33: code finished

--- 33-addTwoNumbers.js
+++ 33-addTwoNumbers.js
@@ -45,36 +45,39 @@ class ListNode {
 // output: 1 array of each digit of added numbers 
 const addTwoNumbers = (l1, l2) => {
   // initialize a dummy node to simplify the code (to a new ListNode)
-
+  const dummy = new ListNode();
   // initialize a variable current to the dummy 
-
+  let current = dummy;
   // initialize a carry variable to 0 to update as you go
-
+  let carry = 0;
   // initialize pointers p1 and p2 to l1 and l2
-
+  let p1 = l1;
+  let p2 = l2;
   // while either p1 or p2 exist, traverse both of them 
-
+  while (p1 || p2) {
     // declare a const x assigned to ternary: either p1 value or 0 (if p1 doesn't exist)
-
+    const x = p1 ? p1.val : 0;
     // declare a const y assigned to ternary: either p2 value or 0 (if p2 doesn't exist)
-
+    const y = p2 ? p2.val : 0;
     // calculate the sum with the carry (x plus y plus carry)
-
+    const sum = x + y + carry;
     // update carry for next iteration (because if there are extra digits, carry them over)
     // reassign carry to Math.floor of the sum divided by 10 
-
+    carry = Math.floor(sum % 10);
     // assign current.next to a new ListNode, passing in sum % 10 
     // updates next pointer of current node in result linked list to point to newly created node, 
     // effectively adding it to the result
-
+    current.next = new ListNode(sum % 10); 
     // reassign current to current.next
-
+    current = current.next;
     // if p1 exists, reassign p1 to p1.next (move forward to next nodes) (same with p2)
-
+    if (p1) p1 = p1.next;
+    if (p2) p2 = p2.next;
+  }
   // if there is a remaining carry (greater than 0), add it as a new node (current.next) 
-
+  if (carry > 0) current.next = new ListNode(carry);
   // return dummy.next - the result linked list (starting from next node of dummy node)
-
+  return dummy.next;
 };
 
 /* // TESTS:
