223fc95a165b9b5e0a616bc100529aa4973af6c0
Katherine Fry <katgfry@me.com>
2024-03-19 14:14:07 -0400

    34: pseudocode strategy

--- 34-maxProfit.js
+++ 34-maxProfit.js
@@ -17,12 +17,30 @@ Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
 */
 
+/* OVERALL STRATEGY:
+Iterate through the array of prices while keeping track of the minimum price so far and
+the maximum profit achievable. 
+*/
+
 /* 
 input: array of integers (prices of given stocks)
 output: number (maximum profit you can achieve on this transaction)
 */
 const maxProfit = prices => {
-  // 
+  // initialize a variable minPrice to the first price in the array 
+
+  // initialize a variable maxPrice to 0 to be updated later
+
+  // iterate through the prices array starting at the second day (index 1 instead of 0)
+
+    // declare a const potentialProf assigned to the prices array at i minus the minPrice
+
+    // update maxProfit: reassign it to the Math.max of maxProfit and potentialProf
+
+    // update minPrice: reassign it to Math.min of minPrice and prices at i 
+
+  // return maxProfit 
+  
 };
 
 /* // TESTS:
