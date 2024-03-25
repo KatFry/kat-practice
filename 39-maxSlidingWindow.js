0d6bd65f96a023ca4c937db6560e1f84c8d7e731
Katherine Fry <katgfry@me.com>
2024-03-25 15:46:18 -0400

    39: strategy/pseudocode

--- 39-maxSlidingWindow.js
+++ 39-maxSlidingWindow.js
@@ -26,6 +26,30 @@ Output: [1]
 // input: array (integers nums), number (sliding window k) 
 // output: array (the max sliding window)
 const maxSlidingWindow = (nums, k) => {
+  // initialize a constant result to an empty array
+
+  // initialize a constant deque to an empty array 
+
+  // iterate over the nums array
+
+    // remove elements from deque that are outside the window 
+    // while the deque length is greater than 0 and deque at 0 is less than i minus k + 1
+
+      // shift the elements
+
+    // remove elements from deque that are smaller than current element 
+    // while deque length is greater than 0 and nums at i >= nums[deque[deque length - 1]]
+
+      // pop the elements 
+
+    // add current index to the queue (push i to deque)
+
+    // if window reaches max size, add maximum element to result
+    // if i greater than or equal to k minus 1 
+
+      // push nums at deque at 0 to the result array
+
+  // return the result array 
 
 };
 
