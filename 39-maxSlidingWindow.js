e7c9b730e74ba26666c432774c12431db91e65f6
Katherine Fry <katgfry@me.com>
2024-03-25 15:50:49 -0400

    39: code finished

--- 39-maxSlidingWindow.js
+++ 39-maxSlidingWindow.js
@@ -27,30 +27,34 @@ Output: [1]
 // output: array (the max sliding window)
 const maxSlidingWindow = (nums, k) => {
   // initialize a constant result to an empty array
-
+  const result = [];
   // initialize a constant deque to an empty array 
-
+  const deque = [];
   // iterate over the nums array
-
+  for (let i = 0; i < nums.length; i++) {
     // remove elements from deque that are outside the window 
     // while the deque length is greater than 0 and deque at 0 is less than i minus k + 1
-
+    while (deque.length > 0 && deque[0] < i - k + 1) {
       // shift the elements
-
+      deque.shift();
+    }
     // remove elements from deque that are smaller than current element 
     // while deque length is greater than 0 and nums at i >= nums[deque[deque length - 1]]
-
+    while (deque.length > 0 && nums[i] >= nums[deque[deque.length - 1]]) {
       // pop the elements 
-
+      deque.pop();
+    }
     // add current index to the queue (push i to deque)
-
+    deque.push(i);
     // if window reaches max size, add maximum element to result
     // if i greater than or equal to k minus 1 
-
+    if (i >= k - 1) {
       // push nums at deque at 0 to the result array
-
+      result.push(nums[deque[0]]);
+    }
+  }
   // return the result array 
-
+  return result;
 };
 
 /* // TESTS:
