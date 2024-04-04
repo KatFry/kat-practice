b8130afefc6f3e302f195309f36e07bdae181e8a
Katherine Fry <katgfry@me.com>
2024-04-04 17:30:56 -0400

    44: code finished

--- 44-isSameTree.js
+++ 44-isSameTree.js
@@ -25,11 +25,26 @@ class TreeNode {
 }
 
 const isSameTree = (p, q) => {
-
+  // if both trees are null, then they are identical (return true)
+  if (!p && !q) return true;
+  // if one tree is null and the other is not, they are not identical (return false)
+  if (!p || !q) return false;
+  // if the values of the current nodes are not equal, they are not identical (return false)
+  if (p.val !== q.val) return false;
+  // recursively check if left and right subtrees are identical (call isSameTree, passing in left and right)
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
 
 /* // TESTS:
+const p1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+const q1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+console.log(isSameTree(p1, q1)); // Output: true
 
+const p2 = new TreeNode(1, new TreeNode(2), null);
+const q2 = new TreeNode(1, null, new TreeNode(2));
+console.log(isSameTree(p2, q2)); // Output: false
 
-
+const p3 = new TreeNode(1, new TreeNode(2), new TreeNode(1));
+const q3 = new TreeNode(1, new TreeNode(1), new TreeNode(2));
+console.log(isSameTree(p3, q3)); // Output: false
 */
