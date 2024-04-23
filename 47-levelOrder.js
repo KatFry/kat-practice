e4ee8b1ec1d52eb49c7a339ffcf0e642786c52a8
Katherine Fry <katgfry@me.com>
2024-04-23 11:49:29 -0400

    47: code finished

--- 47-levelOrder.js
+++ 47-levelOrder.js
@@ -30,29 +30,32 @@ class TreeNode {
 
 const levelOrder = root => {
   // if the root is null, return an empty array 
-
+  if (!root) return [];
   // declare a const result assigned to an empty array and a queue assigned to passing in root
-
+  const result = [];
+  const queue = [root];
   // while the queue length is greater than 0
-
+  while (queue.length > 0) {
     // assign a const levelSize to the length of the queue (num of nodes at current level)
-
+    const levelSize = queue.length;
     // assign a const currentLevel to an empty array (to store vals at current level)
-
+    const currentLevel = [];
     // traverse all nodes at current level with for loop (loop over levelSize) 
-
+    for (let i = 0; i < levelSize.length; i++) {
       // declare a const currentNode assigned to shifting (dequeue) the first node
-
+      const currentNode = queue.shift();
       // push the currentNode val into the currentLevel array 
-
+      currentLevel.push(currentNode.val);
       // if left child exists, enqueue it to the queue 
-
+      if (currentNode.left) queue.push(currentNode.left);
       // if right child exists, enqueue it to the queue 
-
+      if (currentNode.right) queue.push(currentNode.right);
+    }
     // push the currentLevel's values to the result array 
-
+    result.push(currentLevel);
+  }
   // return the result outside the loops 
-  
+  return result;
 };
 
 /* // TESTS:
