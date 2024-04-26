76aad036fcf7f77c4d3f00b5cc6884af40389f11
Katherine Fry <katgfry@me.com>
2024-04-26 16:25:19 -0400

    50: code finished

--- 50-isValidBst.js
+++ 50-isValidBst.js
@@ -33,15 +33,15 @@ class TreeNode {
 // pass in along with the root a min variable initialized to null and a max variable initialized to null
 const isValidBst = root => {
   // base case: if the root is null, return true (it is a valid bst)
-
+  if (!root) return true;
   // if min !== null and root val <= min OR max !== null and root val >= max, return false 
-
+  if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) return false;
   // recursively check left subtree by calling isValidBst and passing in root.left, min, and root.val
-
+  const leftIsValid = isValidBst(root.left, min, root.val);
   // recursively check right subtree by calling isValidBst and passing in root.right, root.val, and max
-
+  const rightIsValid = isValidBst(root.right, root.val, max);
   // return the result of checking if both left and right subtrees are valid 
-  
+  return leftIsValid && rightIsValid;
 };
 
 /* // TESTS:
