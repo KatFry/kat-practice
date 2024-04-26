c199085e8ee016458841fb6f9c1286cd748c7dd1
Katherine Fry <katgfry@me.com>
2024-04-26 16:40:13 -0400

    51: question and tests

---
+++ 51-kthSmallest.js
@@ -0,0 +1,30 @@
+/* 
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all 
+the values of the nodes in the tree.
+
+Example 1:
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+
+Example 2:
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+*/
+
+const kthSmallest = root => {
+
+};
+
+/* // TESTS:
+const root1 = new TreeNode(3,
+  new TreeNode(1, new TreeNode(2)),
+  new TreeNode(4),
+);
+console.log(kthSmallest(root1, 1)); // output: 1
+
+const root2 = new TreeNode(5,
+  new TreeNode(3, new TreeNode(2, new TreeNode(1)), new TreeNode(4)),
+  new TreeNode(6),
+);
+console.log(kthSmallest(root2, 3)); 
+*/
