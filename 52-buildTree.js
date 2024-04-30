486148b4734cb07d2489fee88928f12b598dd1e0
Katherine Fry <katgfry@me.com>
2024-04-30 14:02:11 -0400

    52: buildTree pseudocode

--- 52-buildTree.js
+++ 52-buildTree.js
@@ -28,6 +28,27 @@ class TreeNode {
 }
 
 const buildTree = (preorder, inorder) => {
+  // if there are no nodes to construct (preorder or inorder are empty), then return null
+
+  // declare a const rootValue assigned to preorder at index 0 (first element is the root)
+
+  // declare a const root assigned to a new TreeNode, passing in the rootValue
+
+  // declare a const rootIndexInInorder assigned to indexOf root in the inorder array
+
+  // declare a const leftInorder assigned to slicing inorder at 0 and rootIndexInInorder (left subtree inorder)
+
+  // declare a const rightInorder assigned to slicing inorder at rootIndexInInorder plus 1 (right subtree inorder)
+
+  // declare a const leftPreorder assigned to slicing preorder at 1 and 1 plus leftInorder length (left subtree preorder)
+
+  // declare a const rightPreorder assigned to slicing preorder at 1 plus leftInorder length (right subtree preorder)
+
+  // assign root.left to invoking buildTree, passing in leftPreorder and leftInorder
+
+  // assign root.right to invoking buildTree, passing in rightPreorder and rightInorder
+
+  // return the root node
 
 };
 
